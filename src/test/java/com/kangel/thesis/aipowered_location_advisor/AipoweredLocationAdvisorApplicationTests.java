e3097b06e73aeba0782b5eb93a46d486522c160f
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-01-20 18:16:39 +0200

    Initialize Spring Boot application

---
+++ src/test/java/com/kangel/thesis/aipowered_location_advisor/AipoweredLocationAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangel.thesis.aipowered_location_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipoweredLocationAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
