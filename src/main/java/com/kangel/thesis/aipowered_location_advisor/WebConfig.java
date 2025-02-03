3bfe86f8fba2b009d3cd494b25676e00240bc5b4
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-02-03 02:39:15 +0200

    Added support for CORS from the react frontend

---
+++ src/main/java/com/kangel/thesis/aipowered_location_advisor/WebConfig.java
@@ -0,0 +1,19 @@
+package com.kangel.thesis.aipowered_location_advisor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow CORS from the react frontend
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
