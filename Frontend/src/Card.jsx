3264d66d12bb31c9a7fcc766efedafedda5830fa
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-02-08 02:03:40 +0200

    Test page

---
+++ Frontend/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return (
+        <>
+        <div className="card-container">
+            <h1>Card</h1>
+            <p>This is a card</p>
+        </div>
+        </>
+    )
+}
+
+export default Card;
