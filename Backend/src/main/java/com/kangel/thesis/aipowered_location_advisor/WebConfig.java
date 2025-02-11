9431687b00f4b5091f8db4a74eeb11fe1be0db15
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-02-11 03:21:24 +0200

    Changed some minor stuff and the hosting ip so its accessible to the network devices

--- Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/WebConfig.java
+++ Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow CORS from the react frontend
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://192.168.1.70:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
