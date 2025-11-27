ef8da4229be7ed8dfb227daf40a781aa4a8e0045
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-11-27 19:37:07 +0200

    Added some extra env for prod ready code

--- Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/Services/Authentication/OAuth2/OAuth2GoogleService.java
+++ Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/Services/Authentication/OAuth2/OAuth2GoogleService.java
@@ -64,7 +64,7 @@ public class OAuth2GoogleService extends OAuth2Service {
                         "code", code,
                         "client_id", env.get("GOOGLE_CLIENT_ID"),
                         "client_secret", env.get("GOOGLE_CLIENT_SECRET"),
-                        "redirect_uri", "https://qyvmzivriyxf.share.zrok.io",
+                        "redirect_uri", env.get("GOOGLE_REDIRECT_URI"),
                         "grant_type", "authorization_code"))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, String>>() {
