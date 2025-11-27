8d462cf9027020bc2391de3c01542638112bb7d1
Konstantinos Angelakis <angelakis.ntinos@gmail.com>
2025-11-27 21:18:09 +0200

    Dockerfile implementation and more fixes for production

--- Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/Config/Security/CorsConfig.java
+++ Backend/src/main/java/com/kangel/thesis/aipowered_location_advisor/Config/Security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://qyvmzivriyxf.share.zrok.io/", "http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
